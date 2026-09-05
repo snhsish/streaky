a0fe7bbbaffcb2d2e0ffa3f10b748f9c725471ee
snehasishcodes <snehasishcodes@gmail.com>
2026-09-05 19:36:04 +0530

    chore: setup NativeWind, MMKV, Zustand and deps

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import '../global.css';
 
 import { useColorScheme } from '@/components/useColorScheme';
 
